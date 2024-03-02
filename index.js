1f44d1452cee5b7566881846ff235e3bd43cce66
FELIX CHERUIYOT <felix@intasend.com>
2024-03-02 03:53:17 +0000

    Update workflows

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios')
+
+function request_review(codeData) {
+    axios.post("url", {code: codeData, lang:"python"}, {headers: {ContentType: "application/json"}})
+    .then((resp)=>{
+        return resp.data
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const userResponse = core.getInput('review_code');
-  console.log(`Response ${userResponse}!`);
-  const results = (new Date()).toTimeString();
-  core.setOutput("results", results);
+  const files = core.getInput('files');
+  console.log(`Changed files ${files}!`);
+
+  for (let fileItem = 0; fileItem <files.length; fileItem++){
+    console.log(`Files: ${fileItem}`)
+  }
+
+//   request_review(files[0])
+  
+//   const results = (new Date()).toTimeString();
+  core.setOutput("results", "results");
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }

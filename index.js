65f6eb7be2b8f5da2df2889efd20f509b3f6549f
FELIX CHERUIYOT <felix@intasend.com>
2024-03-02 03:06:16 +0000

    Deploy

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ try {
   // `who-to-greet` input defined in action metadata file
   const userResponse = core.getInput('review_code');
   console.log(`Response ${userResponse}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const results = (new Date()).toTimeString();
+  core.setOutput("results", results);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
